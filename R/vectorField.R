ce514dcca0408a6e83f559bbeec127cffcf58cb9
Christof Schoetz <cschoetz@pik-potsdam.de>
2022-10-07 21:36:29 +0200

    plotVectorField vs plotStateSpaceWithVectorField

--- R/vectorField.R
+++ R/vectorField.R
@@ -1,5 +1,5 @@
 #' @export
-plotVectorField <- function(trajs, fun, parms, title) {
+plotStateSpaceWithVectorField <- function(trajs, fun, parms, title) {
   d <- ncol(trajs$state)
 
   if (d == 2) {
@@ -81,6 +81,70 @@ plotVectorField <- function(trajs, fun, parms, title) {
     theme(legend.position = "none") +
     coord_fixed(ratio = 1) +
     ggtitle(title)
+
+  return(plt)
+}
+
+#' @export
+plotVectorField <- function(derivTrajs, title) {
+
+  subtitle <- NULL
+
+  d <- ncol(derivTrajs$state)
+  if (d == 2) {
+    state <- derivTrajs$state
+    deriv <- derivTrajs$deriv
+  } else {
+    id <- do.call(paste, c(as.list(as.data.frame(derivTrajs$state[,1:2])), sep="_"))
+    resState <- by(derivTrajs$state[,1:2], id, \(df) lapply(df, mean))
+    resDeriv <- by(derivTrajs$deriv[,1:2], id, \(df) lapply(df, mean))
+    state <- matrix(unlist(resState), ncol=2, byrow=TRUE)
+    deriv <- matrix(unlist(resDeriv), ncol=2, byrow=TRUE)
+    subtitle <- "dim3 mean for dim1&2"
+  }
+
+  pltData <- tibble::tibble(
+    x = state[,1],
+    y = state[,2],
+    vx = deriv[,1],
+    vy = deriv[,2])
+
+  pltData <-
+    pltData |>
+    mutate(speed = sqrt(.data$vx^2 + .data$vy^2))
+
+  nArrows <- 20
+  maxSpeed <- max(pltData$speed)
+  if (!is.finite(maxSpeed) || maxSpeed <= .Machine$double.eps)
+    maxSpeed <- .Machine$double.eps
+  sizeX <- max(diff(range(pltData$x))) / nArrows
+  sizeY <- max(diff(range(pltData$y))) / nArrows
+  size <- mean(c(sizeX, sizeY)) * sqrt(2)
+
+  plt <-
+    ggplot(pltData) +
+    geom_segment(
+      aes(
+        x = .data$x,
+        y = .data$y,
+        color = .data$speed,
+        xend = .data$x+.data$vx/maxSpeed*size,
+        yend = .data$y+.data$vy/maxSpeed*size),
+      arrow = arrow(length = grid::unit(0.1, "cm")),
+      size = 0.6) +
+    scale_colour_continuous(
+      low = "grey80",
+      high = "darkred") +
+    xlab(NULL) +
+    ylab(NULL)
+  plt <-
+    plt +
+    theme(legend.position = "none") +
+    coord_fixed(ratio = 1) +
+    ggtitle(
+      title,
+      subtitle = subtitle
+    )
 
   return(plt)
 }
