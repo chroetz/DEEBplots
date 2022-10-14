cfd38a0f606bf0eb78d3477c5e0ba6234c26e8fc
Christof Schoetz <cschoetz@pik-potsdam.de>
2022-10-14 17:44:22 +0200

    new follow time plot

--- R/followTime.R
+++ R/followTime.R
@@ -6,27 +6,16 @@ plotFollowTime <- function(followTime, title) {
 
   data <- tibble(
     time = followTime$time,
-    maxAllowedDistance = pmax(followTime$diffConst * followTime$constFraction, followTime$closeToConstThreshold),
-    distance = followTime$diffFollower,
-    cumTimeLost = followTime$cumTimeLost)
+    distance = followTime$distance)
 
-  plt1 <-
+  plt <-
     ggplot(data, aes(
       x = .data$time,
       y = .data$distance
     )) +
     geom_line() +
-    geom_line(aes(y = .data$maxAllowedDistance), linetype="dashed", color = "red")
-  plt2 <-
-    data |>
-    ggplot(aes(
-      x = .data$time,
-      y = .data$cumTimeLost
-    )) +
-    geom_line() +
-    geom_hline(yintercept=followTime$lostTimeThreshold, linetype="dashed", color = "red") +
+    geom_hline(yintercept=followTime$radius, linetype="dashed", color = "red") +
     geom_vline(xintercept=followTime$followTime, color="blue", linetype="dashed")
-  plt <- gridExtra::arrangeGrob(plt1, plt2, ncol=1, top=title)
 
   return(plt)
 }
